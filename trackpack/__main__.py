f946d4cb9ad35c6f7f042cf54c2ab9343f9bc190
offa <bm-dev@yandex.com>
2021-01-07 14:18:41 +0100

    Year updated.

--- trackpack/__main__.py
+++ trackpack/__main__.py
@@ -1,6 +1,6 @@
 # trackpack - Package audio tracks
 #
-# Copyright (C) 2020  offa
+# Copyright (C) 2020-2021  offa
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
