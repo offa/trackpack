1bf5c486b4e8ff152a017d6fc4c15a385505ea83
offa <bm-dev@yandex.com>
2021-02-18 20:20:12 +0100

    Exception handling moved out of function.

--- trackpack/__main__.py
+++ trackpack/__main__.py
@@ -16,34 +16,35 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
-from trackpack import cli
-from trackpack import config
+
+from trackpack import cli, config
 from trackpack.trackpacker import TrackPacker
+
 
 def __fail(msg):
     print("ERROR: {}".format(msg))
     sys.exit(1)
 
 def __read_config(filename):
-    try:
-        with open(filename, "r") as config_file:
-            cfg = config.Config()
-            cfg.load_from_yaml(config_file)
-            return cfg
-    except FileNotFoundError:
-        __fail("'{}' not found".format(filename))
+    with open(filename, "r") as config_file:
+        cfg = config.Config()
+        cfg.load_from_yaml(config_file)
+        return cfg
 
 
 def main():
     args = cli.parse_args(sys.argv[1:])
 
     if args.command == 'pack':
-        cfg = __read_config("pack.yml")
-        cfg.load_from_cli_args(args)
+        try:
+            cfg = __read_config("pack.yml")
+            cfg.load_from_cli_args(args)
 
-        trackpacker = TrackPacker(cfg.name, cfg.export_dir)
-        (_, stems) = trackpacker.discover_audiofiles(args.pack_explicit_files)
-        trackpacker.pack_files(cfg.archive_name, stems)
+            trackpacker = TrackPacker(cfg.name, cfg.export_dir)
+            (_, stems) = trackpacker.discover_audiofiles(args.pack_explicit_files)
+            trackpacker.pack_files(cfg.archive_name, stems)
+        except FileNotFoundError as ex:
+            __fail("{}: {}".format(ex.strerror, ex.filename))
 
 
 if __name__ == '__main__':
