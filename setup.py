18962c4fe05d4d91455a8bdc96c45f0d7675fcc3
offa <bm-dev@yandex.com>
2021-06-07 17:21:56 +0200

    setup.py formatted.

--- setup.py
+++ setup.py
@@ -7,23 +7,21 @@ def long_description_readme():
         return readme_file.read()
 
 
-setuptools.setup(
-    name='trackpack',
-    version=__version__,
-    author='offa',
-    author_email='offa@github',
-    description='Package audio tracks.',
-    long_description=long_description_readme(),
-    long_description_content_type="text/markdown",
-    url='https://github.com/offa/trackpack',
-    packages=['trackpack'],
-    entry_points={"console_scripts": ["trackpack = trackpack.__main__:main"]},
-    keywords=['audio', 'packaging', 'stems'],
-    classifiers=[
-        'Environment :: Console',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Topic :: Multimedia :: Sound/Audio',
-        'Topic :: Utilities',
-    ],
-    install_requires=['PyYAML']
-)
+setuptools.setup(name='trackpack',
+                 version=__version__,
+                 author='offa',
+                 author_email='offa@github',
+                 description='Package audio tracks.',
+                 long_description=long_description_readme(),
+                 long_description_content_type="text/markdown",
+                 url='https://github.com/offa/trackpack',
+                 packages=['trackpack'],
+                 entry_points={"console_scripts": ["trackpack = trackpack.__main__:main"]},
+                 keywords=['audio', 'packaging', 'stems'],
+                 classifiers=[
+                     'Environment :: Console',
+                     'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+                     'Topic :: Multimedia :: Sound/Audio',
+                     'Topic :: Utilities',
+                 ],
+                 install_requires=['PyYAML'])
